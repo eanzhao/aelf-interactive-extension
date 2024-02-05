18a556aecfc4ae8de26d6d2b3b78a35b049773e9
eanzhao <yiqi.zhao@aelf.io>
2024-02-05 16:50:46 +0800

    Improve commands.

---
+++ modules/AElf.HdWallet/ExtendedKey.cs
@@ -0,0 +1,26 @@
+using NBitcoin;
+
+namespace AElf.HdWallet;
+
+public class ExtendedKey
+{
+    private readonly ExtKey _extKey;
+
+    private ExtendedKey(ExtKey extKey)
+    {
+        _extKey = extKey;
+    }
+
+    internal static ExtendedKey From(ExtKey extKey)
+    {
+        return new ExtendedKey(extKey);
+    }
+
+    public PrivateKey PrivateKey => _extKey.PrivateKey.Wrap();
+    public string Mnemonic { get; set; }
+
+    public ExtendedKey Derive(uint index)
+    {
+        return new ExtendedKey(_extKey.Derive(index));
+    }
+}
--- src/AElfExtensionDemo/AElfExtension.cs
+++ src/AElfExtensionDemo/AElfExtension.cs
@@ -1,5 +1,7 @@
 ﻿using System.CommandLine;
 using AElf.Client;
+using AElf.HdWallet;
+using Google.Protobuf.Reflection;
 using Microsoft.AspNetCore.Html;
 using Microsoft.DotNet.Interactive;
 using Microsoft.DotNet.Interactive.Formatting;
@@ -11,25 +13,14 @@ public static class AElfKernelExtension
 {
     public static void Load(Kernel kernel)
     {
-        var chainOption = new Option<string>(["-i", "--chain"],
-            "Query chain information.");
-        var txOption = new Option<string>(["-t", "--tx"],
-            "Query transaction information.");
-        var contractOption = new Option<string>(["-c", "--contract"],
-            "Query contract information.");
+        var aelfCommand = InitAElfCommand();
 
-        var aelfCommand = new Command("#!aelf", "Query information from aelf blockchain.")
-        {
-            chainOption,
-            txOption,
-            contractOption
-        };
+        var infoCommand = InitInfoCommand();
 
-        aelfCommand.SetHandler(
-            (chain, tx, contract) => KernelInvocationContext.Current.Display(Query(chain, tx, contract)),
-            chainOption,
-            txOption,
-            contractOption);
+        var accountCommand = InitAccountCommand();
+
+        aelfCommand.AddCommand(infoCommand);
+        aelfCommand.AddCommand(accountCommand);
 
         kernel.AddDirective(aelfCommand);
 
@@ -37,7 +28,7 @@ public static class AElfKernelExtension
             code(nameof(AElfExtensionDemo)),
             " is loaded. It helps you query information from aelf node",
             ". Try it by running: ",
-            code("#!aelf -c"),
+            code("#!aelf -c {contractAddress}"),
             " or ",
             code("#!aelf -t {txId}")
         );
@@ -45,11 +36,99 @@ public static class AElfKernelExtension
         KernelInvocationContext.Current?.Display(view);
     }
 
-    private static IHtmlContent Query(string chain, string txId, string contractAddress)
+    private static Command InitAElfCommand()
+    {
+        var txOption = new Option<string>(["-t", "--tx"],
+            "Query transaction information.");
+        var contractOption = new Option<string>(["-c", "--contract"],
+            "Query contract information.");
+
+        var aelfCommand = new Command("#!aelf", "Query information from aelf blockchain.")
+        {
+            txOption,
+            contractOption
+        };
+
+        aelfCommand.SetHandler(txId => KernelInvocationContext.Current.Display(QueryTransactionResult(txId)), txOption);
+        aelfCommand.SetHandler(
+            contractAddress => KernelInvocationContext.Current.Display(QueryContractMethods(contractAddress)),
+            contractOption);
+
+        return aelfCommand;
+    }
+
+    private static Command InitInfoCommand()
+    {
+        var infoCommand = new Command("info", "Query chain status.");
+        infoCommand.SetHandler(_ => KernelInvocationContext.Current.Display(QueryChainStatus()));
+        return infoCommand;
+    }
+
+    private static Command InitAccountCommand()
+    {
+        var accountCommand = new Command("account", "Manage aelf accounts.");
+
+        var passwordOption = new Option<string>(["-p", "--password"], getDefaultValue: () => "aelftest",
+            "Provide the password of new aelf account keystore file.");
+        var newCommand = new Command("new", "Create a new aelf account");
+
+        newCommand.SetHandler(password =>
+        {
+            var extKey = new AElfWalletFactory().Create(passphrase: password);
+            KernelInvocationContext.Current.Display(PrintAccount(extKey));
+        }, passwordOption);
+
+        accountCommand.AddCommand(newCommand);
+
+        return accountCommand;
+    }
+
+    private static IHtmlContent PrintAccount(ExtendedKey extKey)
+    {
+        return div(
+            p($"Private Key: {extKey.PrivateKey.ToHex()}"),
+            p($"Public Key: {extKey.PrivateKey.PublicKey}"),
+            p($"Address: {extKey.PrivateKey.PublicKey.ToAddress()}"),
+            p($"Mnemonic: {extKey.Mnemonic}")
+        );
+    }
+
+    private static IHtmlContent QueryChainStatus()
     {
         var client = new AElfClientBuilder().UseEndpoint("https://tdvw-test-node.aelf.io").Build();
         var chainStatus = client.GetChainStatusAsync().Result;
-        return div($"ChainId: {chainStatus.ChainId}\nHeight: {chainStatus.BestChainHeight}\n" +
-                   $"Hash: {chainStatus.BestChainHash}");
+        return div(
+            p($"ChainId: {chainStatus.ChainId}"),
+            p($"Height: {chainStatus.BestChainHeight}"),
+            p($"Hash: {chainStatus.BestChainHash}")
+        );
+    }
+
+    private static IHtmlContent QueryTransactionResult(string txId)
+    {
+        var client = new AElfClientBuilder().UseEndpoint("https://tdvw-test-node.aelf.io").Build();
+        var txResult = client.GetTransactionResultAsync(txId).Result!;
+
+        return div(
+            p($"TxId: {txResult.TransactionId}"),
+            p($"From: {txResult.Transaction.From}"),
+            p($"To: {txResult.Transaction.To}"),
+            p($"Status: {txResult.Status}"),
+            p($"ReturnValue: {txResult.ReturnValue}")
+        );
+    }
+
+    private static IHtmlContent QueryContractMethods(string contractAddress)
+    {
+        var client = new AElfClientBuilder().UseEndpoint("https://tdvw-test-node.aelf.io").Build();
+        var bytes = client.GetContractFileDescriptorSetAsync(contractAddress).Result;
+        var fileDescriptorSet = FileDescriptorSet.Parser.ParseFrom(bytes);
+        var methods =
+            (from file in fileDescriptorSet.File
+                from service in file.Service
+                from method in service.Method
+                select method.Name).ToList();
+
+        return div(methods.Select(s => p(s)));
     }
 }
